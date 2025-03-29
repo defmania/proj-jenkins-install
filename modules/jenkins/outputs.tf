d0f399be36cee6d5236a6ad6c166ffe83a5d69c4
Bogdan Florea <35240234+defmania@users.noreply.github.com>
2025-03-29 18:13:05 +0200

    removed excess outputs

--- modules/jenkins/outputs.tf
+++ modules/jenkins/outputs.tf
@@ -5,7 +5,3 @@ output "jenkins_ec2_instance_id" {
 output "jenkins_ec2_instance_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
-
-output "jenkins_ec2_instance_connection_string" {
-  value = format("%s%s", "ssh -i /Users/rahulwagh/.ssh/aws_ec2_terraform ubuntu@", aws_instance.jenkins.public_ip)
-}
